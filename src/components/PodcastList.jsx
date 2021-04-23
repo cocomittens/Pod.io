5e9b4fa17d0dd534c44923c18767382aaaf89a74
ioqren <corriestoddard@gmail.com>
2021-04-22 22:04:52 -0700

    Add search functionality

--- src/components/PodcastList.jsx
+++ src/components/PodcastList.jsx
@@ -1,7 +1,8 @@
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, TextField, Typography } from "@material-ui/core";
 import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { Autocomplete } from "@material-ui/lab";
 import PodcastPlayer from "./PodcastPlayer";
 import { fetchPodcast } from "../utils/utils";
 import { makeStyles } from "@material-ui/core/styles";
@@ -18,6 +19,7 @@ const useStyles = makeStyles({
     width: "100%",
     margin: "0",
     padding: "0 1vw",
+    paddingBottom: "10vh",
   },
   header: {
     fontFamily: "raleway",
@@ -29,6 +31,9 @@ const PodcastList = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState(null);
+  const [inputValue, setInputValue] = React.useState("");
+
   const getData = useCallback((url) => {
     fetchPodcast(url).then(function (response) {
       setData((data) => [...data, response]);
@@ -49,6 +54,33 @@ const PodcastList = (props) => {
     dispatch(setEpisodes());
   };
 
+  const handleSelection = (e, newValue) => {
+    setSearch(newValue);
+  };
+
+  const SearchPodcasts = () => {
+    return (
+      <Autocomplete
+        id="combo-box-demo"
+        options={data.map((data) => data.title)}
+        onChange={handleSelection}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        value={search}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            autoFocus={search}
+            label="Search Podcasts..."
+            variant="outlined"
+          />
+        )}
+      />
+    );
+  };
+
   return (
     <div className="App">
       <Grid container className={classes.podcasts} spacing={2} justify="center">
@@ -57,32 +89,48 @@ const PodcastList = (props) => {
             Pod.io
           </Typography>
         </Grid>
-        {data.map((podcast, index) => {
-          return (
-            <Grid
-              container
-              item
-              key={index}
-              xs={12}
-              sm={6}
-              md={4}
-              lg={2}
-              justify="center"
-              alignContent="flex-start"
-            >
-              <Grid item onClick={() => handleClick(podcast)}>
-                <img
-                  className={classes.img}
-                  src={podcast.icon}
-                  alt={podcast.title}
-                />
+        <Grid item xs={12}>
+          <SearchPodcasts />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography className={classes.header} variant="h2">
+            Featured
+          </Typography>
+        </Grid>
+        {data
+          .filter((podcast) => {
+            if (search && podcast.title !== search) {
+              return false;
+            } else {
+              return true;
+            }
+          })
+          .map((podcast, index) => {
+            return (
+              <Grid
+                container
+                item
+                key={index}
+                xs={12}
+                sm={6}
+                md={4}
+                lg={2}
+                justify="center"
+                alignContent="flex-start"
+              >
+                <Grid item onClick={() => handleClick(podcast)}>
+                  <img
+                    className={classes.img}
+                    src={podcast.icon}
+                    alt={podcast.title}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography variant="h6">{podcast.title}</Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography variant="h5">{podcast.title}</Typography>
-              </Grid>
-            </Grid>
-          );
-        })}
+            );
+          })}
       </Grid>
       <PodcastPlayer />
     </div>
